0ccbf17b81b308c1b4d55e50b1d96b29fc7d0477
Joel <69668715+Geakie@users.noreply.github.com>
2021-08-28 21:21:32 +0800

    Removed Creative Mode

--- App/Source/Scene2D/Player2D.cpp
+++ App/Source/Scene2D/Player2D.cpp
@@ -418,30 +418,30 @@ void CPlayer2D::Update(const double dElapsedTime)
 			//	//CS: Change Color
 			//	//currentColor = glm::vec4(1.0, 0.0, 1.0, 0.5);
 			//}
-			if (cKeyboardController->IsKeyPressed(GLFW_KEY_C))
-			{
-				cout << "Creative Mode" << endl;
+			//if (cKeyboardController->IsKeyPressed(GLFW_KEY_C))
+			//{
+			//	cout << "Creative Mode" << endl;
 
-				if (cPhysics2D.GetStatus() == CPhysics2D::STATUS::IDLE)
-				{
-					cPhysics2D.SetStatus(CPhysics2D::STATUS::JUMP);
-					cPhysics2D.SetInitialVelocity(glm::vec2(0.0f, 3.5f));
-					jumpCount += 1;
-					// Play a jump sound
-					cSoundController->PlaySoundByID(3);
-				}
-				else
-				{
-					if (jumpCount < 2)
-					{
-						cPhysics2D.SetStatus(CPhysics2D::STATUS::JUMP);
-						cPhysics2D.SetInitialVelocity(glm::vec2(0.0f, 1.5f));
-						jumpCount += 1;
-						// Play a jump sound
-						cSoundController->PlaySoundByID(3);
-					}
-				}
-			}
+			//	if (cPhysics2D.GetStatus() == CPhysics2D::STATUS::IDLE)
+			//	{
+			//		cPhysics2D.SetStatus(CPhysics2D::STATUS::JUMP);
+			//		cPhysics2D.SetInitialVelocity(glm::vec2(0.0f, 3.5f));
+			//		jumpCount += 1;
+			//		// Play a jump sound
+			//		cSoundController->PlaySoundByID(3);
+			//	}
+			//	else
+			//	{
+			//		if (jumpCount < 2)
+			//		{
+			//			cPhysics2D.SetStatus(CPhysics2D::STATUS::JUMP);
+			//			cPhysics2D.SetInitialVelocity(glm::vec2(0.0f, 1.5f));
+			//			jumpCount += 1;
+			//			// Play a jump sound
+			//			cSoundController->PlaySoundByID(3);
+			//		}
+			//	}
+			//}
 			//debug code
 			if (cKeyboardController->IsKeyPressed(GLFW_KEY_L))
 			{
